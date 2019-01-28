19b72898a01bd86fb660b290dc62c470151ded2f
abu <1611499758@qq.com>
2019-01-28 18:52:18 +0800

    node circleci commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// 建立最簡單的Hello World網頁
+var express = require('express')
+var app = express()
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+var server = app.listen(8081, function () {
+    var port = server.address().port
+    console.log('server is listening at port: ' + port)
+})
